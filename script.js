08d426d29bfd0808aac0676c81ef47a15be2e541
Bogdan <vip.pokupatel@mail.ru>
2020-07-10 14:38:11 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+"use strict";
+
+
+/* var leftBorder = 1;
+let second = 2;
+const pi = 3.14;
+
+let person = {
+    name: 'John',
+    age: 25,
+    isMarrid: false
+}; */
+
+/* console.log(person['name']); */
+
+/* alert('hello'); */
+/* let answer = confirm('are you here?');
+console.log(answer); */
+
+/* let answer = + prompt('есть ли вам 18', "да");
+console.log(answer);
+
+console.log('4' + '- object');
+ */
+
+ let incr = 10,
+     decr = 10;
+
+ /* incr++;
+ decr--; */
+
+/* console.log(++incr);
+console.log(--decr); */
+
+/* console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log("2" == 2);
+ */
+
+ /* let isChecked = true,
+     isClose = true;
+
+console.log(isChecked && isClose); */
+
+/* let isChecked = true,
+     isClose = false;
+
+console.log(isChecked && isClose); */
+
+/* let isChecked = true,
+     isClose = false;
+
+console.log(isChecked || isClose); */
+
+/* let isChecked = false,
+     isClose = false;
+
+console.log(isChecked || isClose); */
+
+let isChecked = false,
+     isClose = false;
+
+console.log(isChecked || !isClose);
